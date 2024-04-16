b7be8f85a0325a6714db9d3797be043b09b86692
hahuang4 <142834501+hahuang4@users.noreply.github.com>
2024-04-16 00:48:18 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -5,15 +5,27 @@ public class Digits
 
 	private ArrayList<Integer> digitList;
 
-	public Digits(int num)
-	{ /* to be implemented in part (a) */ 
-	    
-	}
+public Digits (int num)
+{
+digitList = new ArrayList<Integer> ();
+if (num == 0)
+{
+digitList.add (new Integer (0));
+}
+while (num > 0)
+{
+digitList.add (0, new Integer (num & 10));
+num /= 10;
+}}
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
-		
-
+		int x=0;
+		for(int i=digitlist.size();i>0;i--)
+	if(digitList.get(i)<digitList.get(i-1))	
+x++;
+		if(x>0)
+			return false;
 	}
 	
 	public String toString()
